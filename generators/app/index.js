f6cb56e89eae34af4c5e48777b332168ede61b85
kbulis <kirk.d.bulis@gmail.com>
2017-07-20 15:55:48 -0700

    Updated testing

--- generators/app/index.js
+++ generators/app/index.js
@@ -11,23 +11,23 @@ module.exports = class extends Generator {
     ));
 
     const prompts = [
-      { type: 'input'
-      , name: 'name'
-      , message: 'Your project name'
-      , default: this.appname
+      {type: 'input',
+        name: 'name',
+        message: 'Your project name',
+        default: this.appname
       },
-      { type: 'input'
-      , name: 'description'
-      , message: 'Your project description'
-      , default: 'Just another angular app express api webpack project...'
+      {type: 'input',
+        name: 'description',
+        message: 'Your project description',
+        default: 'Just another angular app express api webpack project...'
       },
-      { type: 'input'
-      , name: 'author'
-      , message: 'Author'
+      {type: 'input',
+        name: 'author',
+        message: 'Author'
       },
-      { type: 'input'
-      , name: 'repository'
-      , message: 'Repository'
+      {type: 'input',
+        name: 'repository',
+        message: 'Repository'
       }
     ];
 
@@ -37,36 +37,31 @@ module.exports = class extends Generator {
   }
 
   writing() {
-    this.fs.copyTpl
-      ( this.templatePath('_package.json'), this.destinationPath('package.json')
-      , { name: this.props.name
-        , description: this.props.description
-        , author: this.props.author
-        , repository: this.props.repository
-        }
-      );
+    this.fs.copyTpl(this.templatePath('_package.json'), this.destinationPath('package.json')
+      , {name: this.props.name,
+        description: this.props.description,
+        author: this.props.author,
+        repository: this.props.repository
+      }
+    );
 
-    this.fs.copyTpl
-      ( this.templatePath('_webpack.config.js'), this.destinationPath('webpack.config.js')
-      , { name: this.props.name
-        }
-      );
+    this.fs.copyTpl(this.templatePath('_webpack.config.js'), this.destinationPath('webpack.config.js')
+      , {name: this.props.name
+      }
+    );
 
-    this.fs.copyTpl
-      ( this.templatePath('_src/**'), this.destinationPath('src')
-      , { name: this.props.name
-        }
-      );
+    this.fs.copyTpl(this.templatePath('_src/**'), this.destinationPath('src')
+      , {name: this.props.name
+      }
+    );
 
-    this.fs.copyTpl
-      ( this.templatePath('_test/**'), this.destinationPath('test')
-      , { name: this.props.name
-        }
-      );
+    this.fs.copyTpl(this.templatePath('_test/**'), this.destinationPath('test')
+      , {name: this.props.name
+      }
+    );
   }
 
   install() {
     this.npmInstall();
   }
-
 };
